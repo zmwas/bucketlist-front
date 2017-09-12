d42be4611cccbc55a9df7e0b762835be2ce65085
Zacharia <zacmwangi94@gmail.com>
2017-09-12 14:12:33 +0300

    [FEATURE]Add action creators for login

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,5 +1,5 @@
 import AuthService from '../api/authService';
-import { SIGN_UPUSER_SUCCESS, SIGN_UPUSER_FAILURE } from './actiontypes';
+import { SIGN_UPUSER_SUCCESS, LOGIN_USER_SUCCESS, SIGN_UPUSER_FAILURE, LOGIN_USER_FAILURE } from './actiontypes';
 
 
 export function createUserSuccess(user) {
@@ -9,6 +9,12 @@ export function createUserFailure(user){
     return { type: SIGN_UPUSER_FAILURE,user}
 }
 
+export function loginUserSuccess(user) {
+  return { type: LOGIN_USER_SUCCESS, user };
+}
+export function loginUserFailure(user){
+    return { type: LOGIN_USER_FAILURE,user}
+}
 
 export function signUp(user) {
   return dispatch => AuthService.post('https://bucketlist-goals-api.herokuapp.com/auth/register',
@@ -21,3 +27,13 @@ export function signUp(user) {
   });
 }
 
+export function login(user) {
+  return dispatch => AuthService.login('https://bucketlist-goals-api.herokuapp.com/auth/login',
+    user, (status, data) => {
+      const token = data.Authorization;
+      window.localStorage.setItem('token', token);
+      return dispatch(loginUserSuccess(status));
+    }).catch((error) => {
+      return dispatch(loginUserFailure(error.response))
+  });
+}
