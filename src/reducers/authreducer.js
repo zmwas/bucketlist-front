a5b71ee86a0d09ff29caf554516c14a4cc65db0a
Zacharia <zacmwangi94@gmail.com>
2017-09-12 14:11:54 +0300

    [FEATURE]Add reducers for login

--- src/reducers/authreducer.js
+++ src/reducers/authreducer.js
@@ -6,7 +6,11 @@ export default function authReducer(state = initialState.user, action) {
   switch (action.type) {
     case types.SIGN_UPUSER_SUCCESS:
       return action.user;
+    case types.LOGIN_USER_SUCCESS:
+      return action.user;
     case types.SIGN_UPUSER_FAILURE:
+      return Object.assign({}, action.user);
+    case types.LOGIN_USER_FAILURE:
       return Object.assign({}, action.user);
 
   }
