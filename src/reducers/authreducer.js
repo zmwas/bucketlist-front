097cbf6bd44a2188820cf9a7749f9f4866990a42
Zacharia <zacmwangi94@gmail.com>
2017-09-12 11:18:44 +0300

    [FEATURE]Add reducer for user signup

---
+++ src/reducers/authreducer.js
@@ -0,0 +1,14 @@
+import * as types from '../actions/actiontypes';
+import initialState from './initalState';
+
+
+export default function authReducer(state = initialState.user, action) {
+  switch (action.type) {
+    case types.SIGN_UPUSER_SUCCESS:
+      return action.user;
+    case types.SIGN_UPUSER_FAILURE:
+      return Object.assign({}, action.user);
+
+  }
+  return state;
+}
