dd7e5e168341bca5fd658b61202c7b6490a00ce0
Zacharia <zacmwangi94@gmail.com>
2017-09-15 12:57:13 +0300

    [REFACTOR]Add module for separating css and js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,16 @@
-const webpack = require('webpack')
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = merge(common, {
     plugins: [
-             new UglifyJSPlugin(),
-             new webpack.DefinePlugin({
+        new UglifyJSPlugin(),
+        new ExtractTextPlugin("[name]-[hash].css"),
+        new webpack.optimize.OccurenceOrderPlugin(),
+
+        new webpack.DefinePlugin({
                    'process.env': {
              'NODE_ENV': JSON.stringify('production')
            }
